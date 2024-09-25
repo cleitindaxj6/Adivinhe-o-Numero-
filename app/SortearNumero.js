1bc054290efd029988bdccc367e031500aa8504a
cleitindaxj6 <134400713+cleitindaxj6@users.noreply.github.com>
2024-09-25 11:31:32 -0300

    Add files via upload

---
+++ app/SortearNumero.js
@@ -0,0 +1,14 @@
+const menorValor = 1;
+const maiorValor = 100;
+const numeroSecreto = gerarNumero()
+
+function gerarNumero() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log(numeroSecreto)
+
+const elementoMenor = document.getElementById('menor-valor')
+elementoMenor.innerHTML = menorValor
+const elementoMaior = document.getElementById("maior-valor")
+elementoMaior.innerHTML = maiorValor
